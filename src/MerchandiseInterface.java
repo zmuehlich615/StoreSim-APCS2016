20cce56069d09b0bd391f58274fce4624089a6bc
David Bradley <david@dbradley771.com>
2016-01-14 09:03:30 -0600

    Updated files from class today.

--- src/MerchandiseInterface.java
+++ src/MerchandiseInterface.java
@@ -1,65 +1,82 @@
-
 public interface MerchandiseInterface {
-	
+	/**
+	 * Set the cost of the item to the customer
+	 */
+	int setCost(double cost);
+
 	/**
 	 * Get the cost of the item to the customer
+	 * 
 	 * @return int value cost
 	 */
 	int getCost();
-	
+
 	/**
-	 * Change the cost of the item to the customer
+	 * Temporarily change the cost of the item to the customer
 	 */
-	void changeCost(int cost);
-	
+	void changeCost(double cost, int time);
+
+	/**
+	 * Set the cost of the item to the store
+	 */
+	int setStoreCost(double cost);
+
 	/**
 	 * Get the cost of the item to the store
+	 * 
 	 * @return int value storeCost
 	 */
-	int getStoreCost();
-	
+	int getCost();
+
 	/**
 	 * Change the cost of the item to the store
 	 */
-	void changeStoreCost(int cost);
-	
+	void changeStoreCost(double cost);
+
+	/**
+	 * Set the date the item will expire based on day from now.
+	 */
+	void setExpiration(int days);
+
 	/**
 	 * Get the date the item will expire
+	 * 
 	 * @return int item expiration
 	 */
 	int getExpiration();
-	
+
 	/**
-	 * Set the level of desirability of an item
-	 * 0 = Low, 2 = High
+	 * Set the level of desirability of an item 0 = Low, 2 = High
 	 */
 	void setDesirability(int desirability);
-	
+
 	/**
-	 * Get the level of desirability of an item
-	 * 0 = Low, 2 = High
+	 * Get the level of desirability of an item 0 = Low, 2 = High
+	 * 
 	 * @return int value desirability
 	 */
 	int getDesirability();
-	
+
 	/**
 	 * Set the name of an item
 	 */
 	void setName(String name);
-	
+
 	/**
 	 * Get the name of an item
+	 * 
 	 * @return String item name
 	 */
 	String getName();
-	
+
 	/**
 	 * Set the amount of stock you have of an item
 	 */
 	void setQuantity(int quantity);
-	
+
 	/**
 	 * Get the amount of stock you have of an item
+	 * 
 	 * @return int item quantity
 	 */
 	int getQuantity();
