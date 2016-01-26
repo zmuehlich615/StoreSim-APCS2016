6808f382abd7bf880340430f263b1248c51e5eca
ckauth97 <collinsgenericemail@gmail.com>
2016-01-26 08:44:29 -0600

    Removed the get functions from these classes, are not needed for the constructiors

--- src/CannedAppleSauce.java
+++ src/CannedAppleSauce.java
@@ -2,17 +2,12 @@
 public class CannedAppleSauce extends Food{
 	public CannedAppleSauce {
 		setCost(.7);
-		getCost();
 		changeCost(.5, 7);
 		setStoreCost(.2);
-		getStoreCost();
 		setExpiration(365);
-		getExpiration();
 		setDesirability(0);
-		getDesirability();
 		setName("Canned Apple Sauce");
-		String getName();
 		setQuantity(100);
-		getQuantity();
+		
 	}
 }
