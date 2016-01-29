db5cbeeb1a2203384bc871e707fa50091e3d42bd
zmuehlich615 <zmuehlich615@millardps.org>
2016-01-29 08:23:45 -0600

    3 Foods - Apples, Bananas, Popcorn

---
+++ src/Apples.java
@@ -0,0 +1,12 @@
+package interfaces.food;
+
+public class Apples extends Food{
+	public Apples() {
+		setName("Apples");
+		setCost(1.5);
+		setStoreCost(2.99);
+		setQuantity(30);
+		setExpiration(7);
+		setDesirability(2);
+	}
+}
---
+++ src/Bananas.java
@@ -0,0 +1,13 @@
+package interfaces.food;
+
+public class Bananas extends Food {
+	public Bananas() {
+		setName("Bananas");
+		setCost(1.99);
+		setStoreCost(3.99);
+		setQuantity(30);
+		setExpiration(7);
+		setDesirability(2);
+	}
+}
+
