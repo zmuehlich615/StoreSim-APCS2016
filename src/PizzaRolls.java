de6c4c8d71047a131f1f571043b3cf27c42c649d
sosborn002 <sosborn002@millardps.org>
2016-01-29 08:32:36 -0600

    Added both food classes Hot Pockets and Pizza Rolls all values set, comments put in per the need, was told to ignore any errors.

---
+++ src/PizzaRolls.java
@@ -0,0 +1,13 @@
+package interfaces.food;
+
+public class PizzaRolls extends Food {
+	
+	public void PizzaRolls(){
+		setName("Pizza Rolls");
+		setCost(5);
+		setStoreCost(2);
+		setQuantity(20);
+		setExpiration(5); // days
+		setDesirability(2);
+	}
+}
