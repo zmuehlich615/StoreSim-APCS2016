0c6b0875ced814edf04a1922f2e0f3778f3191fc
csnodgrass <csnodgrass233@millardps.org>
2016-01-29 08:26:35 -0600

    Added Pie Class and Pie Mix Class Might want to check this

---
+++ PieMix.java
@@ -0,0 +1,11 @@
+package interfaces.food;
+public class PieMix  {
+public PieMix(){
+	setDesirability(1);
+	setCost(2.00);
+	setStoreCost(4.00);
+	setQuantity(15);
+	setExpiration(186);
+	setName("Pie Mix");
+}
+}
