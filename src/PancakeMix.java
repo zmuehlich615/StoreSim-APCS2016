7720c73e8055f58106faede99e77509302068e3c
njones958 <nickjns2010@gmail.com>
2016-01-29 08:24:48 -0600

    Adding Two Foods to the game -- Nick Jones
    Frozen Pizza & Pancake Mix

---
+++ src/PancakeMix.java
@@ -0,0 +1,11 @@
+
+public class PancakeMix extends food{
+	public PancakeMix{
+		setName("Pancake Mix");
+		setCost(3.99);
+		setStoreCost(1.72);
+		setQuantity(11);
+		setExpiration(30);
+		setDesirability(2);
+	}
+}
