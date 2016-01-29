04715f4d0ed7d6ba0730f851df2c77eacc6cba41
dbeut <dbeutler329@millardps.org>
2016-01-29 08:41:47 -0600

    Added Food Classes Created beef, blueberries and strawberries.

---
+++ src/Blueberries.java
@@ -0,0 +1,16 @@
+package org.millardps.StoreSim;
+
+import interfaces.food.Food;
+
+
+public class Blueberries extends Food {
+	public Blueberries(){
+		setDesirability(1);
+		setExpiration(7);
+		setName("Blueberries");
+		setCost(3.99);
+		setStoreCost(1.50);
+		setQuantity(5);
+		}
+}
+//
---
+++ src/Strawberries.java
@@ -0,0 +1,16 @@
+package org.millardps.StoreSim;
+
+import interfaces.food.Food;
+
+
+public class Strawberries extends Food {
+	public Strawberries(){
+		setDesirability(2);
+		setExpiration(7);
+		setName("Strawberries");
+		setCost(4.99);
+		setStoreCost(1.50);
+		setQuantity(15);
+		}
+}
+//
